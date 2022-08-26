478b974625d4dacc5bbcd4935bf061125012147a
YoussefMennani <youssefmennani1996@gmail.com>
2022-08-26 10:30:04 +0100

    first commit dao

---
+++ src/main/java/com/spring/dto/Model/Owner.java
@@ -0,0 +1,37 @@
+package com.spring.dto.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String cine;
+
+
+    @OneToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(name = "owner_id")
+    private List<Car> cars = new ArrayList<>();
+
+
+
+
+}
